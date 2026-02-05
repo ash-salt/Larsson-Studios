792d546b564bdfa683895f8ca976283565d69734
Ash <joel.fagersand@gmail.com>
2026-02-05 16:01:26 +0100

    Add floor tiles, maybe not useful but better than not having them

--- Assets/Scripts/player_actions/SweepAttack.cs
+++ Assets/Scripts/player_actions/SweepAttack.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
-
-public class SweepAttack : MonoBehaviour
+/*
+public class SweepAttack : MonoBehaviour, Action
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    int damage;
+
+    public int getCost()
     {
-        
+        return 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void act(MapTile tile1, MapTile tile2, MapTile tile3)
     {
-        
+        MapTile[] tiles = {tile1, tile2, tile3};
+        foreach (MapTile tile in tiles)
+        {
+            GameObject target = tile.getContainedObject();
+            if (target is Enemy)
+            {
+                var enemy = target as Enemy;
+                enemy.takeDamage(damage);
+            }   
+        }
     }
 }
+*/
