5b51ebcbf23c2d4f1fab87164ae78eb434aeaf37
Joel Fagersand <Joel Fagersand>
2026-02-16 15:30:07 +0100

    start work on actions and system

---
+++ Assets/Scripts/player_actions/BlockAction.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+
+namespace Assets.Scripts.player_actions
+{
+	public class BlockAction: MonoBehaviour, IAction
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		public int getCost()
+		{
+			return 1;
+		}
+
+		public void execute(GameObject target)
+		{
+            if (target.TryGetComponent<PlayerScript>(out PlayerScript player))
+            {
+                player.SetBlocking(true);
+            }
+
+        }
+	
+	}
+}
---
+++ Assets/Scripts/player_actions/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Assets.Scripts.player_actions
+{
+	public class PlayerScript: EntityScript
+	{
+		Boolean isBlocking = false;
+
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		public void SetBlocking(Boolean isBlocking)
+		{
+			this.isBlocking = isBlocking;
+		}
+	}
+}
