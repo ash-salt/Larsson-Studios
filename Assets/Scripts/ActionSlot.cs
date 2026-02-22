9cd3ddbbdab7ce0c716f13b506bcefa627391e4c
Markus Rynner <markusry@chalmers.se>
2026-02-22 20:38:27 +0100

    added shield prefab and fixed ui

--- Assets/Scripts/ActionSlot.cs
+++ Assets/Scripts/ActionSlot.cs
@@ -4,18 +4,22 @@ using UnityEngine.UI;
 public class ActionSlot : MonoBehaviour
 {
     private Sprite actionSprite;
-
+    [SerializeField] private Sprite defaultSprite;
     [SerializeField] public Image actionImage;
 
+    public void Awake()
+    {
+        ClearActionSprite();
+    }
     public void SetActionSprite(Sprite newSprite)
     {
         actionSprite = newSprite;
-        actionImage.sprite = actionSprite;
+        actionImage.sprite = newSprite;
     }
     public void ClearActionSprite()
     {
         actionSprite = null;
-        actionImage.sprite = null;
+        actionImage.sprite = defaultSprite;
     }
 
     public Sprite GetActionSprite()
