ccc6735b163752b996e906907e6cc273b8ef1298
Ash <joel.fagersand@gmail.com>
2026-02-24 13:53:42 +0100

    add tooltip functionality

---
+++ Assets/Scripts/ToolTipManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ToolTipManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject main;
+    public GameObject other1;
+    public GameObject other2;
+    public void SetToolTipVisibility()
+    {
+        bool isActive = main.activeSelf;
+
+        main.SetActive(!isActive);
+        other1.SetActive(false);
+        other2.SetActive(false);
+    }
+}
