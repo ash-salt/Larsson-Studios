b1df7dcca6ebdfa5a1b0729728ee2137ddfc73ed
Joel Fagersand <Joel Fagersand>
2026-02-25 13:50:50 +0100

    add start to goblin ranged attacks

---
+++ Assets/Scripts/GoblinRangedAttack.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GoblinRangedAttack : IAction 
+{
+    public int getCost()
+    {
+        return 2;
+    }
+
+    public void execute(EntityScript entity)
+    {
+        
+    }
+}
