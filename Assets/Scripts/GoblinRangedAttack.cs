875591812602a99aa0c55625c1bec6e1fd2a2ff3
Joel Fagersand <Joel Fagersand>
2026-02-27 09:58:58 +0100

    add animation object to gamestatemanager and start on goblinarcher

--- Assets/Scripts/GoblinRangedAttack.cs
+++ Assets/Scripts/GoblinRangedAttack.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class GoblinRangedAttack : IAction 
 {
+    private bool prepared = false;
     public int getCost()
     {
-        return 2;
+        return 1;
     }
 
     public void execute(EntityScript entity)
     {
-        
+        if (prepared == false)
+        {
+            prepared = true;
+        }
+        else
+        {
+            
+        }
     }
 }
